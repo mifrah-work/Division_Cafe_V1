040e272d5a07af667cf86fc349ac3e2e37bb989d
dellulli <177489710+bbokari121@users.noreply.github.com>
2026-01-17 10:51:26 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Division_Cafe_V1/'
+  base: '/Divisixon_Cafe_V1/'
 })
